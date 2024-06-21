92a77e2bcc741e61e25a12b9a07cea08f2e4cd03
Vahid Khodadadi <khodadadi_vahid@yahoo.com>
2024-06-21 13:12:29 +0330

    Created gomoku component with a table

---
+++ src/components/Gomoku.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+type Color = 'BLACK' | 'WHITE';
+
+const squares = reactive<{color: Color | null}[][]>(Array(15).fill(Array(15).fill({ color: null })));
+console.log('squares', squares);
+
+const color = ref<Color>('WHITE');
+
+
+const selectSquare = (i: number , j: number) => {
+    console.log(i, j);
+    
+}
+
+</script>   
+
+<template>
+    <table>
+        <tbody>
+            <tr v-for="(row, i) in squares" class="row">
+                <td v-for="(square, j) in row">
+                    <span v-if="!!square.color" class="circle" style="{ background-color: square.color; }"></span>
+                    <span @click="() => selectSquare(i, j)" v-else></span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<style scoped>
+table {
+    border: 1px solid black;
+    border-collapse: collapse;
+    table-layout: fixed;    
+}
+
+td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    width: 30px;
+    height: 30px;
+    box-sizing: border-box;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Gomoku from './Gomoku.vue';
+
+defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+  <Gomoku />
+</template>
+
+<style scoped>
+
+</style>
