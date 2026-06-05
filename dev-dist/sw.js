f7b9d12a38475980f2c28177bbf0730dbd5c047a
Vahid Khodadadi <khodadadi_vahid@yahoo.com>
2026-06-05 16:56:29 +0330

    fix: styles.css

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.07mlsb5lisg"
+    "revision": "0.lea7b7n4ro8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
